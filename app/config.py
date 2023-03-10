702a4732e6268457e9b2311145e196ae649548c4
unknown <milanmartis@gmail.com>
2023-03-10 10:23:13 +0100

    fcsm commit 7

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 import os
 
+
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245Dret70Nm1255Ui452eeOp125Bnryexx7895'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://xxazizetlirofs:d391fd470610436ff0c3091be22724db7034cb636ed424d2794e7074a91cc302@ec2-34-226-11-94.compute-1.amazonaws.com:5432/dugtf9j1q681m'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'milanmartis@gmail.com'
+    MAIL_PASSWORD = 'iighwhrmbkxxaazk'
