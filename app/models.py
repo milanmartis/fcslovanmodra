238ef775be2d94e9493481f30e224edd6a75efb5
unknown <milanmartis@gmail.com>
2023-03-11 12:15:58 +0100

    v34

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ import uuid
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
