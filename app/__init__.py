3dd4372b4c62a6a5abeab3147f96fd9de3b53ee5
unknown <milanmartis@gmail.com>
2023-04-04 09:39:03 +0200

    club-2023-04-04-c

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    bootstrap.init_app(app)
+    # bootstrap.init_app(app)
 
     from app.users.routes import users
     from app.posts.routes import posts
