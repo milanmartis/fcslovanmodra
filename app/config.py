6f0ec3c010439dbf0fadc4c82edee5d250467275
unknown <milanmartis@gmail.com>
2023-04-08 11:22:02 +0200

    c1

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245Dret70Nm1255Ui452eeOp125Bnryexx7895'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///fcsm.db'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://jjtebpcxjhphei:6e6471c9ed987cb9afad268cf13ef38a25c05e304c8a22656c5625b8cbb2aa57@ec2-3-208-74-199.compute-1.amazonaws.com:5432/d6rllm2kitp5nf'
+    # SQLALCHEMY_DATABASE_URI = 'postesql://bjgwuwmontdjxx:867570147e6ec9e0a17e4a6e98996354c3c4a4b73a46093f627f33e3cf24300c@ec2-44-208-206-97.compute-1.amazonaws.com:5432/d64vv023g375ce'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
