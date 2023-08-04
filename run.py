9df9aefc2122adfb2de8fbe84cae29481084b4b2
unknown <milanmartis@gmail.com>
2023-08-05 00:00:47 +0200

    2023-08-04-13

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port = port)
+        app.run(host='0.0.0.0', port = port, use_reloader=True)
