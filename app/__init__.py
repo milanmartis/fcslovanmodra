8b652ab65db33f5923c981fd47d53ed0cc0ea8b9
unknown <milanmartis@gmail.com>
2023-10-04 13:38:56 +0200

    v2023-10-04-2

--- app/__init__.py
+++ app/__init__.py
@@ -63,12 +63,12 @@ def create_app(config_class=Config):
 
 
     
-    # @app.before_request
-    # def before_request():
-    #     if not request.is_secure:
-    #         url = request.url.replace('http://', 'https://', 1)
-    #         code = 301
-    #         return redirect(url, code=code)
+    @app.before_request
+    def before_request():
+        if not request.is_secure:
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
     from .models import User, Role
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
