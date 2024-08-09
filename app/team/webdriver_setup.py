06e74fb9112d2876a107a9ee622a9f94513ebcd4
unknown <milanmartis@gmail.com>
2024-08-09 15:43:36 +0200

    v2024-8-9-3

--- app/team/webdriver_setup.py
+++ app/team/webdriver_setup.py
@@ -3,18 +3,13 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 def get_webdriver():
-    # Nastavení cest k binárkám, které poskytuje Heroku
-    chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
-    chrome_driver_bin = os.environ.get('CHROMEDRIVER_PATH', "/app/.chromedriver/bin/chromedriver")
-
-    # Nastavení možností pro Chrome
     chrome_options = Options()
-    chrome_options.binary_location = chrome_bin
-    chrome_options.add_argument("--headless")  # Spuštění Chrome v headless režimu
+    chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN')
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_driver_path = os.getenv('CHROMEDRIVER_PATH')
 
-    # Inicializace WebDriveru
-    driver = webdriver.Chrome(executable_path=chrome_driver_bin, options=chrome_options)
+    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
     return driver
