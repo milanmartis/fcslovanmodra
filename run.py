b25f8bdbbd84b0bf04870ea595188e940f4fd1ac
unknown <milanmartis@gmail.com>
2026-03-16 10:34:26 +0100

    v26-3-16-3

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 import os
 from flask_socketio import SocketIO
 from app import create_app
@@ -9,10 +6,9 @@ app = create_app()
 
 socketio = SocketIO(
     app,
-    async_mode="eventlet",
+    async_mode="threading",
     cors_allowed_origins="*",
     logger=True,
-    
     engineio_logger=True,
 )
 
